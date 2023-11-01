d5d7e0cb43382f890cd11fd5a76fa30533e0ca8d
kiluchandran <kiluchandran@gmail.com>
2023-11-01 19:24:24 +0530

    Added patterns_1.py

---
+++ just_the_basics/patterns_1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+print("kilu chandran")
+print("Aswin Babu K")
+print('o----')
+print(' |||| ')
+print('*' * 10)
+print(' * ' * 10)
+print(' kilu ' * 10)
+print('kilu' * 10)
+price = 10
+price = 20
+print(price)
+rating = 2.3
+print(rating)
+name = 'Kilu'
+print(name)
+is_published = True
+print(is_published)
