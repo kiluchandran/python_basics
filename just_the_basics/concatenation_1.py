afc29d2fc0b7e2709a9e4f1a173c628a7ed89e73
kiluchandran <kiluchandran@gmail.com>
2023-11-02 17:59:19 +0530

    Added concatenation_1.py

---
+++ just_the_basics/concatenation_1.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+name = input('what is your name? ')
+print('hi ' + name)
+
+name = input('what is your name? ')
+color = input('what is your favourite color? ')
+print(name +' likes ' +color)
+
+
+first = 'kilu'
+last = 'chandran'
+message = first + ' [ ' + last + ' ] is a coder '
+msg = f' {first} [{last}] is a coder '
+print(message)
+print(msg)
+
+first = 'aswin'
+middle = 'babu'
+last = 'k'
+msg = first + ' [ ' + middle + ' ] ' + ' [ ' + last + '] is a coder'
+message = f' {first} [{middle}] [{last}] is a coder'
+print(msg)
+print(message)
