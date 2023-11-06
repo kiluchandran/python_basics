983db962ab9110f546f4bb5550166dd8a684efef
kiluchandran <kiluchandran@gmail.com>
2023-11-06 19:48:13 +0530

    Added string_methods1.py

---
+++ just_the_basics/string_methods1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+course = ' Python for Begineers '
+print(course.upper())
+print(course.lower())
+print(course.find('Be'))
+print(course.replace('Begineers','Absolute Begineers'))
+print('Python' in course)
+print(course)
