b7aed004e1cb939f10df64486e40e63165209c11
kiluchandran <kiluchandran@gmail.com>
2023-11-07 17:49:29 +0530

    Added types_1.py

---
+++ just_the_basics/types_1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+birth_year = input('birth year: ')
+age = 2022 - int(birth_year)
+print(age)
+print(type(birth_year))
+print(type(age))
+
+weight_in_pounds = input('what is your weight in pounds? ')
+weight_in_kgs = float(weight_in_pounds) * 0.453592
+print(weight_in_kgs)
+
+print(int(116.845))
